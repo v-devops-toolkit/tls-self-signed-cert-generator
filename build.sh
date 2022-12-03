ae4b9bf0f20631466c41e5438e0f57313a405585
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2022-12-03 12:51:48 +0100

    refactor: bash functions

--- build.sh
+++ build.sh
@@ -1,11 +1,61 @@
 #!/usr/bin/env bash
 
+function clean_dirs() {
+  rm -rf ca_directory
+  rm -rf server
+}
 
-# TODO add if param is not set
+function init_dirs() {
+  mkdir -p ca_directory
+  cat openssl.cnf.template | sed -e "s/XXX-TEMPLATE-DOMAIN-XXX/${1}/g" >ca_directory/openssl.cnf
+  mkdir -p ca_directory/certs
+  mkdir -p ca_directory/private
+  chmod 700 ca_directory/private
+  echo 01 >ca_directory/serial
+  touch ca_directory/index.txt
+}
 
-DOMAIN=$1
+function gen_ca() {
+  cd ca_directory
 
-./clean-dirs.sh
-./init-dirs.sh ${DOMAIN}
-./gen-ca.sh ${DOMAIN}
-./gen-server.sh ${DOMAIN}
+  openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 \
+      -out ${1}.cacert.pem -outform PEM -subj /CN=${1}/ -nodes
+
+  cd ..
+}
+
+function gen_server() {
+
+  mkdir -p server
+  cd server
+
+  # the command to generate the key file
+  openssl genrsa -out ${1}.key.pem 2048
+
+  openssl req -new -key ${1}.key.pem -out ${1}.req.pem -outform PEM \
+      -subj /CN=${1}/O=server/ -nodes
+
+  cd ../ca_directory
+  openssl ca -config openssl.cnf -in ../server/${1}.req.pem -out \
+      ../server/${1}.cert.pem -notext -batch -extensions server_ca_extensions
+  cd ..
+
+  rm server/${1}.req.pem
+}
+
+
+if ! [ -x "$(command -v openssl)" ]; then
+  echo "Command not found: openssl"
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+  echo Missing required parameter: domain
+  exit 2
+fi
+
+clean_dirs
+init_dirs ${1}
+gen_ca ${1}
+gen_server ${1}
+
